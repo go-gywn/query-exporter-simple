f7e07558d4154813960879efbb387e67edf71947
gywndi <gywndi@gmail.com>
2021-07-13 14:09:09 +0900

    modify readme and change some code

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # query-exporter-simple
+
+Simple query exporter to explain
+
+## Build & Run
+```bash
+$ go build .
+```
+
+## Config example
+```yaml
+dsn: test:test123@tcp(127.0.0.1:3306)/information_schema
+metrics:
+  process_count_by_host:
+    query: "select user, substring_index(host, ':', 1) host, count(*) sessions from information_schema.processlist group by 1,2 "
+    type: gauge
+    description: "process count by host"
+    labels: ["user","host"]
+    value: sessions
+  process_count_by_user:
+    query: "select user, count(*) sessions from information_schema.processlist group by 1 "
+    type: gauge
+    description: "process count by user"
+    labels: ["user"]
+    value: sessions
+```
+
+## Run
+```bash
+$ ./query-exporter --bind="0.0.0.0:9104" --config="config.yml"
+INFO[0000] Regist version collector - query_exporter    
+INFO[0000] metric description for "process_count_by_host" registerd 
+INFO[0000] metric description for "process_count_by_user" registerd 
+INFO[0000] HTTP handler path - /metrics                 
+INFO[0000] Starting http server - 0.0.0.0:9104  
+```
+
+## Metrics
+```bash
+curl 127.0.0.1:9104/metrics 
+# HELP go_gc_duration_seconds A summary of the pause duration of garbage collection cycles.
+# TYPE go_gc_duration_seconds summary
+go_gc_duration_seconds{quantile="0"} 0
+go_gc_duration_seconds{quantile="0.25"} 0
+
+.. skip ..
+
+# HELP go_threads Number of OS threads created.
+# TYPE go_threads gauge
+go_threads 7
+# HELP query_exporter_build_info A metric with a constant '1' value labeled by version, revision, branch, and goversion from which query_exporter was built.
+# TYPE query_exporter_build_info gauge
+query_exporter_build_info{branch="",goversion="go1.16.5",revision="",version=""} 1
+# HELP query_exporter_process_count_by_host process count by host
+# TYPE query_exporter_process_count_by_host gauge
+query_exporter_process_count_by_host{host="localhost",user="event_scheduler"} 1
+query_exporter_process_count_by_host{host="localhost",user="test"} 1
+# HELP query_exporter_process_count_by_user process count by user
+# TYPE query_exporter_process_count_by_user gauge
+query_exporter_process_count_by_user{user="event_scheduler"} 1
+query_exporter_process_count_by_user{user="test"} 1
+```
+
+Enjoy your own exporter!!
--- main.go
+++ main.go
@@ -9,28 +9,28 @@ import (
 	"strconv"
 	"strings"
 
-	_ "github.com/go-sql-driver/mysql"
-
 	"github.com/ghodss/yaml"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/version"
 	log "github.com/sirupsen/logrus"
 )
 
+var config Config
+
 const (
-	name = "query_exporter"
+	collector = "query_exporter"
 )
 
 func main() {
 	var err error
-	var config Config
-
+	var configFile, bind string
 	// =====================
 	// Get OS parameter
 	// =====================
-	var configFile string
 	flag.StringVar(&configFile, "config", "config.yml", "configuration file")
+	flag.StringVar(&bind, "bind", "0.0.0.0:9104", "bind")
 	flag.Parse()
 
 	// =====================
@@ -49,27 +49,14 @@ func main() {
 	}
 
 	// ========================
-	// Regist describe
-	// ========================
-	for metricName, metric := range config.Metrics {
-		metric.Labels = append(metric.Labels, "instance")
-		metric.metricDesc = prometheus.NewDesc(
-			prometheus.BuildFQName(name, config.Type, metricName),
-			metric.Description,
-			metric.Labels, nil,
-		)
-		config.Metrics[metricName] = metric
-		log.Infof("metric description for \"%s\" registerd", metricName)
-	}
-
-	// ========================
 	// Regist handler
 	// ========================
-	prometheus.MustRegister(&QueryCollector{
-		cfg: &config,
-	}, version.NewCollector(name))
+	log.Infof("Regist version collector - %s", collector)
+	prometheus.Register(version.NewCollector(collector))
+	prometheus.Register(&QueryCollector{})
 
 	// Regist http handler
+	log.Infof("HTTP handler path - %s", "/metrics")
 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		h := promhttp.HandlerFor(prometheus.Gatherers{
 			prometheus.DefaultGatherer,
@@ -78,8 +65,8 @@ func main() {
 	})
 
 	// start server
-	log.Infof("Starting http server - %s", config.Bind)
-	if err := http.ListenAndServe(config.Bind, nil); err != nil {
+	log.Infof("Starting http server - %s", bind)
+	if err := http.ListenAndServe(bind, nil); err != nil {
 		log.Errorf("Failed to start http server: %s", err)
 	}
 }
@@ -88,11 +75,8 @@ func main() {
 // Config config structure
 // =============================
 type Config struct {
-	Bind     string
-	Type     string
-	DSN      string
-	Instance string
-	Metrics  map[string]struct {
+	DSN     string
+	Metrics map[string]struct {
 		Query       string
 		Type        string
 		Description string
@@ -105,33 +89,43 @@ type Config struct {
 // =============================
 // QueryCollector exporter
 // =============================
-type QueryCollector struct {
-	cfg *Config
-}
+type QueryCollector struct{}
 
 // Describe prometheus describe
 func (e *QueryCollector) Describe(ch chan<- *prometheus.Desc) {
+	for metricName, metric := range config.Metrics {
+		metric.metricDesc = prometheus.NewDesc(
+			prometheus.BuildFQName(collector, "", metricName),
+			metric.Description,
+			metric.Labels, nil,
+		)
+		config.Metrics[metricName] = metric
+		log.Infof("metric description for \"%s\" registerd", metricName)
+	}
 }
 
 // Collect prometheus collect
 func (e *QueryCollector) Collect(ch chan<- prometheus.Metric) {
-	// ========================
+
 	// Connect to database
-	// ========================
-	db, err := sql.Open(e.cfg.Type, e.cfg.DSN)
+	db, err := sql.Open("mysql", config.DSN)
 	if err != nil {
-		log.Errorf("Connect to %s database failed: %s", e.cfg.Type, err)
+		log.Errorf("Connect to database failed: %s", err)
 		return
 	}
 	defer db.Close()
 
-	for _, metric := range e.cfg.Metrics {
+	// Execute each queries in metrics
+	for name, metric := range config.Metrics {
+
+		// Execute query
 		rows, err := db.Query(metric.Query)
 		if err != nil {
 			log.Errorf("Failed to execute query: %s", err)
 			continue
 		}
 
+		// Get column info
 		cols, err := rows.Columns()
 		if err != nil {
 			log.Errorf("Failed to get column meta: %s", err)
@@ -152,11 +146,6 @@ func (e *QueryCollector) Collect(ch chan<- prometheus.Metric) {
 				data[cols[i]] = string(bytes)
 			}
 
-			// upsert instance
-			if data["instance"] == "" {
-				data["instance"] = e.cfg.Instance
-			}
-
 			// Metric labels
 			labelVals := []string{}
 			for _, label := range metric.Labels {
@@ -170,9 +159,10 @@ func (e *QueryCollector) Collect(ch chan<- prometheus.Metric) {
 			switch strings.ToLower(metric.Type) {
 			case "counter":
 				ch <- prometheus.MustNewConstMetric(metric.metricDesc, prometheus.CounterValue, val, labelVals...)
-			case "guage":
+			case "gauge":
 				ch <- prometheus.MustNewConstMetric(metric.metricDesc, prometheus.GaugeValue, val, labelVals...)
 			default:
+				log.Errorf("Fail to add metric for %s: %s is not valid type", name, metric.Type)
 				continue
 			}
 		}
